89f76e62c81efb953fe960ff8816932d03499566
kinikoro <kinikoro96@yahoo.com>
2023-06-15 18:10:27 -0700

    modified

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
  * main - Entry point
  * Return: Always 0 (success)
  */
 int main(void)
 {
-        puts("/"programming is like building a multilingual puzzle");
-        return (0);
+puts("/"programming is like building a multilingual puzzle");
+return (0);
 }
