cadda31ebad0e76a41d92bda5df37fe491116175
kinikoro <kinikoro96@yahoo.com>
2023-06-16 21:39:58 -0700

    modified

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,19 +1,12 @@
 #include<stdio.h>
+#include<stdio.h>
 /**
  * main - Entry point
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float f;
-printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
-printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
-printf("size of a long int: %lu bytes(s)\n", (unsigned long)sizeof(c));
-printf("size of a long long int: %lu bytes(s)\n", (unsigned long)sizeof(d));
-printf("size of a float: %lu bytes(s)\n", (unsigned long)sizeof(f));
-return (0);
-{
+	write(2,
+	"and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
